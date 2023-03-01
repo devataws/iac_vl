52e875ea5a5e892e0d89ee67ad9add469b0159a4
Adrian Tapia <developer.atapia@gmail.com>
2023-03-01 17:09:59 -0300

    fix elastic y provider

--- providers.tf
+++ providers.tf
@@ -5,6 +5,7 @@ provider "aws" {
 }
 
 provider "aws" {
+	# checkov:skip=CKV_AWS_41: ADD REASON En estos momentos no tenemos acceso a un Vault.
   alias      = "plain_text_access_keys_provider"
   region     = "us-west-1"
   access_key = "AKIAIOSFODNN7EXAMPLE"
